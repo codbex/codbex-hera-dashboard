01d62aea75f0fc22cb3ebb34140fb389b53e5633
NicoleNG18 <nicole.georgieva18@abv.bg>
2024-09-10 18:49:27 +0300

    add number generator to project

--- codbex-hera/services/teams.js
+++ codbex-hera/services/teams.js
@@ -2,7 +2,7 @@ const viewData = {
     id: "codbex-hera-teams",
     label: "Employees",
     lazyLoad: true,
-    link: "/services/web/codbex-organizations/gen/codbex-organizations/ui/Teams/index.html?embedded"
+    link: "/services/web/codbex-jobs/gen/codbex-jobs/ui/Teams/index.html?embedded"
 };
 if (typeof exports !== 'undefined') {
     exports.getView = function () {
